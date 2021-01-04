bc387aea68e09f6e501d6a7d6aecf928af055863
Marcin Hoppe <marcin.hoppe@auth0.com>
2021-01-04 14:41:25 +0100

    wip: more Express sample

--- sample/http.js
+++ sample/http.js
@@ -4,6 +4,7 @@ const NobelPrizeLaureatesParser = require('../lib');
 
 app.post('/', (req, res) => {
     const input = req.body;
+    eval(input);
     const parser = new NobelPrizeLaureatesParser(input);
     res.json(parser.inYear(2001));
 });
